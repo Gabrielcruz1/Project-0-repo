f9f7510b5ab1c817efe207022dbf047d57bb2739
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-10 12:58:54 -0800

    added variables to js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+console.log("hello world")
+// buttons 
+const sleepButton = document.querySelector('#Btn1');
+const hungryButton = document.querySelector('#Btn2');
+const boredButton = document.querySelector('#Btn3');
+const startButton = document.querySelector('#Btn5');
+// health indicators
+const sleepHealth = document.querySelector('#sleepy');
+const hungryHealth = document.querySelector('#hungry');
+const boredHealth =  document.querySelector('#bored');
