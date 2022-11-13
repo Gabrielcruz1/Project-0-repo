d6004d4d2a394aa7e9c3bb1c5507345da45250e6
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-13 10:07:36 -0800

    updated css style

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const meter1 = document.querySelector('.restMeter');
+let meter1 = document.querySelector('.restMeter');
 const meter2 = document.querySelector('.hungryMeter')
 const meter3 = document.querySelector('.boredMeter')
 
@@ -10,27 +10,31 @@ const startButton = document.querySelector('#startGame');
 
 // make our rest meter , hungry meter , and bored meter go down automatically
 
-let restDecreaser = 0 
 
-function myFunction (){
-    document.querySelector(".restMeter").stepDown()
+function lowerRest (){
+    if(meter1 === 10){
+    meter1 -=1}
+    meter1.innerHTML = `rest meter: ${meter1}`
 }
+
+
+
 
 let restRaiser = 0
 let hungryRaiser = 0
 let boredRaiser = 0
 
 restButton.addEventListener('click', () => {
-    restRaiser +=1
+    restRaiser -=1
     meter1.innerHTML = `rest meter: ${restRaiser} `
 })
 
 hungryButton.addEventListener('click', () => {
-    hungryRaiser +=1
+    hungryRaiser -=1
     meter2.innerHTML = `hungry meter: ${hungryRaiser}`
 })
 
 boredButton.addEventListener('click', () => {
-    boredRaiser +=1
+    boredRaiser -=1
     meter3.innerHTML = `bored meter: ${boredRaiser}`
 })
