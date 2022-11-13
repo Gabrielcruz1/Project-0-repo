44400fef4b1673a909ea9e1a0e9784b2e32596cb
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-13 11:23:04 -0800

    added set interval function for all 4 meters

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
-const meter1 = document.querySelector('.restMeter');
+const meter1 = document.querySelector('.restMeter')
 const meter2 = document.querySelector('.hungryMeter')
 const meter3 = document.querySelector('.boredMeter')
+const meter4 = document.querySelector('.ageMeter')
 
 const restButton = document.querySelector('#restButton');
 const hungryButton = document.querySelector('#hungryButton');
@@ -11,22 +12,23 @@ const startButton = document.querySelector('#startGame');
 let restRaiser = 0
 let hungryRaiser = 0
 let boredRaiser = 0
+let ageRaiser = 0
 
-// function lowerRest (){
-//     if(meter1 === 10){
-//     meter1 -=1}
-//     meter1.innerHTML = `rest meter: ${meter1}`
-// }
 
-// setInterval(() =>{
+// 
+setInterval(() =>{
+    meter1.innerHTML = `Rest meter: ${restRaiser++} `
+    meter2.innerHTML = `Hungry meter: ${hungryRaiser++}`
+    meter3.innerHTML = `Bored meter: ${boredRaiser++}`
+    meter4.innerHTML = `🐯 age: ${ageRaiser++}`
 
-// }, 1000)
+}, 1000)
 
 
 // below are functions that decrease the meters 
 restButton.addEventListener('click', () => {
     restRaiser -=1
-    meter1.innerHTML = `Rest meter: ${restRaiser} `
+    meter1.innerHTML = `Rest meter: ${restRaiser}`
 })
 
 hungryButton.addEventListener('click', () => {
