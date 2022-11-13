795c62de7edfe4c1634ed4985db3f7fc579afddd
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-13 08:53:59 -0800

    updated html

--- main.js
+++ main.js
@@ -8,22 +8,29 @@ const hungryButton = document.querySelector('#hungryButton');
 const boredButton = document.querySelector('#boredButton');
 const startButton = document.querySelector('#startGame');
 
+// make our rest meter , hungry meter , and bored meter go down automatically
 
-// let restRaiser = 0
-// let hungryRaiser = 0
-// let boredRaiser = 0
+let restDecreaser = 0 
 
-// restButton.addEventListener('click', () => {
-//     restRaiser +=1
-//     meter1.innerHTML = `rest meter: ${restRaiser} `
-// })
+function myFunction (){
+    document.querySelector(".restMeter").stepDown()
+}
 
-// hungryButton.addEventListener('click', () => {
-//     hungryRaiser +=1
-//     meter2.innerHTML = `hungry meter: ${hungryRaiser}`
-// })
+let restRaiser = 0
+let hungryRaiser = 0
+let boredRaiser = 0
 
-// boredButton.addEventListener('click', () => {
-//     boredRaiser +=1
-//     meter3.innerHTML = `bored meter: ${boredRaiser}`
-// })
+restButton.addEventListener('click', () => {
+    restRaiser +=1
+    meter1.innerHTML = `rest meter: ${restRaiser} `
+})
+
+hungryButton.addEventListener('click', () => {
+    hungryRaiser +=1
+    meter2.innerHTML = `hungry meter: ${hungryRaiser}`
+})
+
+boredButton.addEventListener('click', () => {
+    boredRaiser +=1
+    meter3.innerHTML = `bored meter: ${boredRaiser}`
+})
