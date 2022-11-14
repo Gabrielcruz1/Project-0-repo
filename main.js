fe70b6eb5685328b3baec79673a512862dab4981
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-13 19:55:02 -0800

    updated css

--- main.js
+++ main.js
@@ -1,31 +1,62 @@
-
+// THE METERS SHOWN ON HTML
 const meter1 = document.querySelector('.restMeter')
 const meter2 = document.querySelector('.hungryMeter')
 const meter3 = document.querySelector('.boredMeter')
 const meter4 = document.querySelector('.ageMeter')
 
+// MY BUTTONS STORED IN A VARIABLE
 const restButton = document.querySelector('#restButton');
 const hungryButton = document.querySelector('#hungryButton');
 const boredButton = document.querySelector('#boredButton');
 const startButton = document.querySelector('#startGame');
 
+// INCREMENTER
 let restRaiser = 0
 let hungryRaiser = 0
 let boredRaiser = 0
 let ageRaiser = 0
 
+//A START GAME FUNCTION THAT ALLOWS THE INTERVALS TO BEGIN
+    startButton.addEventListener('click',() => {
+    const timer = setInterval(() =>{   
+        if (restRaiser === 10 || hungryRaiser === 10 || boredRaiser === 10){
+        clearInterval(timer)
+        } 
+        meter1.innerHTML = `Rest meter: ${restRaiser++}`
+        meter2.innerHTML = `Hungry meter: ${hungryRaiser++}`
+        meter3.innerHTML = `Bored meter: ${boredRaiser++}`
+        meter4.innerHTML = `🐯 Age: ${ageRaiser++}`
+     }, 1000)
+     
+ })
 
-// 
-setInterval(() =>{
-    meter1.innerHTML = `Rest meter: ${restRaiser++} `
-    meter2.innerHTML = `Hungry meter: ${hungryRaiser++}`
-    meter3.innerHTML = `Bored meter: ${boredRaiser++}`
-    meter4.innerHTML = `🐯 age: ${ageRaiser++}`
 
-}, 1000)
+function alertFunction(){
+    if(restRaiser === 10 || hungryRaiser === 10 || boredRaiser === 10){
+    alert("hello");
+    }
+}
 
 
-// below are functions that decrease the meters 
+// END GAME FUNCTION THAT KILLS GAME WHEN ANY METER HITS 10
+// function endGame(){
+//     if (meter1 === 10 || meter2 === 10 || meter3 === 10){
+//         clearInterval(timer)
+//     }
+// }
+
+
+
+// BELOW IS SET INTERVAL FUNCTION 
+// setInterval(() =>{   
+//    meter1.innerHTML = `Rest meter: ${restRaiser++}`
+//    meter2.innerHTML = `Hungry meter: ${hungryRaiser++}`
+//    meter3.innerHTML = `Bored meter: ${boredRaiser++}`
+//    meter4.innerHTML = `🐯 Age: ${ageRaiser++}`
+// }, 1000)
+
+
+// BELOW ARE FUNCTIONS FOR BUTTONS THAT DECREASE THE METER
 restButton.addEventListener('click', () => {
     restRaiser -=1
     meter1.innerHTML = `Rest meter: ${restRaiser}`
